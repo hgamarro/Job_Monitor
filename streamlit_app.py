baa11bb9afdb59a8145519e1761717b12fc4a0a3
hgamarro <hgamarro@gmail.com>
2024-11-27 22:04:23 -0500

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 from google.cloud import firestore
-from google.oauth2 import service_account 
+from google.oauth2 import service_account
 import json
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="job_monitor")
+db = firestore.Client(credentials=creds)
+
 doc_ref = db.collection("completed_runs").document("FfncJd")
 doc = doc_ref.get()
 st.write("The id is: ", doc.id)
