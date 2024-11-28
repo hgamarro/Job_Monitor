cb2673bcd94ef1abe46355c87fced885da474726
hgamarro <33673089+hgamarro@users.noreply.github.com>
2024-11-27 22:21:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ from google.cloud import firestore
 from google.oauth2 import service_account
 from google.cloud.firestore_v1._helpers import DatetimeWithNanoseconds
 import json
+from datetime import datetime
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
