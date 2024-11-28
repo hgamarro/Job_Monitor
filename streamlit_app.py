dd9edb84af6f18b8196f77b7ff4114c1ff88b8b2
hgamarro <hgamarro@gmail.com>
2024-11-27 21:35:22 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,13 @@
 import streamlit as st
+from google.cloud import firestore
+import json
 
-st.header('Hello 🌎! :D')
-if st.button('Balloons?'):
-    st.balloons()
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="job_monitor")
+doc_ref = db.collection("completed_runs").document("FfncJd")
+doc = doc_ref.get()
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())
 
 
